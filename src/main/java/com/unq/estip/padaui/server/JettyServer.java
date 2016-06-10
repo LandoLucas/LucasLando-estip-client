eee45756c50cc22e3ac6845480bb94f461fe7cd8
Lucas Lando <lucas.emanuel.lando@everis.com>
2016-06-10 16:49:20 -0300

    tweaking jettyServer config

--- src/main/java/com/unq/estip/padaui/server/JettyServer.java
+++ src/main/java/com/unq/estip/padaui/server/JettyServer.java
@@ -6,17 +6,33 @@ import org.eclipse.jetty.webapp.WebAppContext;
 public class JettyServer {
 
 	 public static void main(String[] args) throws Exception{
-    	 String portStr = System.getenv("PORT");
-         int port = (portStr == null) ? 8085 : Integer.parseInt(portStr);
-         Server server = new Server(port);
-         WebAppContext webapp = new WebAppContext();
-         webapp.setContextPath("/");
-         // change the name of the war as needed.
-         webapp.setWar("target/classes/pada-client.war");
-         server.setHandler(webapp);
+		 String webappDirLocation = "src/main/webapp/";
+	        
+	        //The port that we should run on can be set into an environment variable
+	        //Look for that variable and default to 8080 if it isn't there.
+	        String webPort = System.getenv("PORT");
+	        if(webPort == null || webPort.isEmpty()) {
+	            webPort = "9191";
+	        }
 
-         server.start();
-         server.join();
+	        Server server = new Server(Integer.valueOf(webPort));
+	        WebAppContext root = new WebAppContext();
+
+	        root.setContextPath("/");
+	        root.setDescriptor(webappDirLocation+"/WEB-INF/web.xml");
+	        root.setResourceBase(webappDirLocation);
+	        
+	        //Parent loader priority is a class loader setting that Jetty accepts.
+	        //By default Jetty will behave like most web containers in that it will
+	        //allow your application to replace non-server libraries that are part of the
+	        //container. Setting parent loader priority to true changes this behavior.
+	        //Read more here: http://wiki.eclipse.org/Jetty/Reference/Jetty_Classloading
+	        root.setParentLoaderPriority(true);
+	        
+	        server.setHandler(root);
+	        
+	        server.start();
+	        server.join();   
     }
 	
 }
